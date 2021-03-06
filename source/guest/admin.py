401f2f8c48e6e22d364b7bb0e1e3e5711bb1cc49
Tilekbai <tilek009@mail.ru>
2021-03-06 13:44:34 +0600

    Сгенерирована миграция для модели:"Guestbook"

--- source/guest/admin.py
+++ source/guest/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from guest.models import Guestbook
 
 # Register your models here.
+
+
+class GuestbookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'text', 'status', 'create_time', 'update_time']
+    list_filter = ['name', 'status']
+    search_fields = ['name', 'status']
+    fields = ['name', 'text', 'status']
+
+
+admin.site.register(Guestbook, GuestbookAdmin)
